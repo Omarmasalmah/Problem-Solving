8e230f879e7f911d242a6cc905a2baa22df17b79
Omar <1200060@student.birzeit.edu>
2023-03-15 19:17:31 +0200

    solution

--- Q217- Contains Duplicate/main.py
+++
@@ -1,7 +0,0 @@
-def containsDuplicate(nums):
-    return len(set(nums)) != len(nums)
-
-
-if __name__ == '__main__':
-    nums = [1, 2, 3, 1]
-    print(containsDuplicate(nums))
--- Q242- Valid Anagram/main.py
+++
@@ -1,7 +0,0 @@
-def isAnagram(s, t):
-    return sorted(s) == sorted(t)
-
-if __name__ == "__main__":
-    s = "anagram"
-    t = "nagaram"
-    print(isAnagram(s, t))
